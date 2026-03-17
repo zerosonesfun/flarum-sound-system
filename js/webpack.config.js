7274d41d491decac481ba8a8ce33e49778f48d34
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2026-03-16 20:11:20 -0400

    Fix 1.8.x: webpack externals use global app instead of flarum.reg.get

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,15 @@
 const config = require('flarum-webpack-config');
 
-module.exports = config();
+const base = config();
+
+// Flarum 1.x: app is provided by the parent as global `app`. Run first so we
+// override 2.0-style flarum.reg.get() and the bundle works on 1.8.x.
+function flarumAppExternal(context, request, callback) {
+  if (request === 'flarum/admin/app' || request === 'flarum/forum/app') {
+    return callback(null, 'var app');
+  }
+  callback();
+}
+const externals = [flarumAppExternal].concat(base.externals || []);
+module.exports = Object.assign({}, base, { externals });
 
